04332e2ab15970d38d8acf0489b6b78a4e220537
avreeeb <burn0243@algonquinlive.com>
2015-03-04 17:20:42 -0500

    doneeeee

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ var $diamond = $('.diamond');
 var $btnCollapseExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
 
 $btnShowHide.on('click', function () {
     $box.toggleClass('js-box-dissappear');
@@ -21,3 +26,17 @@ $btnCollapseExpand.on('click', function () {
     $panel.toggleClass('js-panel-expand');
 });
 
+$btnBounce.on('click', function () {
+    $circle.addClass('js-circle-bounce');
+});
+
+$circle.on('webkitAnimationEnd animationend', function () {
+	console.log('fired');
+    $circle.removeClass('js-circle-bounce');
+});
+
+$btnAppend.on('click', function () {
+    var $li = $('<li>New List Item</li>');
+    $list.prepend($li);
+    $li.addClass('js-list-append');
+});
